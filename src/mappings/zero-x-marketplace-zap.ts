cab41aaff63d0a64495d7e911bde0ae769eed8a4
Amaan <amaansayyad@yahoo.com>
2023-04-28 22:15:31 +0530

    Update zero-x-marketplace-zap.ts

--- src/mappings/zero-x-marketplace-zap.ts
+++ src/mappings/zero-x-marketplace-zap.ts
@@ -1,69 +1,61 @@
 import { log } from "@graphprotocol/graph-ts";
-import {
-  Buy,
-  DustReturned,
-  Sell,
-  Swap
-} from "../types/ZeroXMarketplaceZap/ZeroXMarketplaceZap"
 import { createDustReturned, getDustReturned, getMint, getRedeem, getSwap, getUser, getZapBuy, getZapSell, getZapSwap } from "./helpers";
 
-export function handleBuy(event: Buy): void {
-  let txHash = event.transaction.hash;
-  let redeem = getRedeem(txHash, event.address);
-  let zapBuy = getZapBuy(txHash);
-
-  redeem.type = "ZapBuy";
-  redeem.save();
-
-  zapBuy.ethAmount = event.params.ethSpent;
-  zapBuy.vaultAction = redeem.id;
-  zapBuy.save();
-
-  log.info("REDEEM : Redeem.ID = {}", [redeem.id])
-  createDustReturned(txHash, redeem.id);
+interface ZapAction {
+  type: string;
+  ethAmount: BigInt;
+  vaultAction: string;
 }
 
+function handleZapAction(event: ethereum.Event, type: string, ethAmount: BigInt, vaultAction: string, getAction: (txHash: Bytes) => any): void {
+  const txHash = event.transaction.hash;
+  const action = getAction(txHash);
+  const zapAction = {
+    type,
+    ethAmount,
+    vaultAction
+  } as ZapAction;
+
+  if (action) {
+    action.type = type;
+    action.save();
+    zapAction.vaultAction = action.id;
+    zapAction.save();
+    log.info(`${type.toUpperCase()} : ${type.toUpperCase()}.ID = {}`, [action.id]);
+    createDustReturned(txHash, action.id);
+  } else {
+    log.warning(`Warning : ${type} not found; TxHash = {}`, [txHash.toHexString()]);
+  }
+}
+
+export function handleBuy(event: Buy): void {
+  const redeem = getRedeem(event.transaction.hash, event.address);
+  redeem.type = "ZapBuy";
+  redeem.save();
+  handleZapAction(event, "ZapBuy", event.params.ethSpent, redeem.id, getZapBuy);
+}
 
 export function handleSell(event: Sell): void {
-  let txHash = event.transaction.hash;
-  let mint = getMint(txHash, event.address);
-  let zapSell = getZapSell(txHash);
-
+  const mint = getMint(event.transaction.hash, event.address);
   mint.type = "ZapSell";
   mint.save();
-
-  zapSell.ethAmount = event.params.ethReceived;
-  zapSell.vaultAction = mint.id;
-  zapSell.save();
-  log.info("MINT : Mint.ID = {}", [mint.id])
-  createDustReturned(txHash, mint.id);
+  handleZapAction(event, "ZapSell", event.params.ethReceived, mint.id, getZapSell);
 }
 
 export function handleSwap(event: Swap): void {
-  let txHash = event.transaction.hash;
-  let swap = getSwap(txHash,  event.address);
-  let zapSwap = getZapSwap(txHash);
-
+  const swap = getSwap(event.transaction.hash, event.address);
   swap.type = "ZapSwap";
   swap.save();
-
-  zapSwap.ethAmount = event.params.ethSpent;
-  zapSwap.vaultAction = swap.id;
-  zapSwap.save();
-  log.info("SWAP : Swap.ID = {}", [swap.id])
-  createDustReturned(txHash,  swap.id);
+  handleZapAction(event, "ZapSwap", event.params.ethSpent, swap.id, getZapSwap);
 }
 
 export function handleDustReturned(event: DustReturned): void {
-  let txHash = event.transaction.hash;
-  let dustReturned = getDustReturned(txHash);
-  if(dustReturned){
+  const txHash = event.transaction.hash;
+  const dustReturned = getDustReturned(txHash);
+  if (dustReturned) {
     dustReturned.ethAmount = event.params.ethAmount;
     dustReturned.vTokenAmount = event.params.vTokenAmount;
     dustReturned.to = getUser(event.params.to).id;
     dustReturned.save();
-  }
-  else {
-    log.warning("Warning : DustReturned not found; TxHash = {}", [txHash.toHexString()])
-  }
-}
+  } else {
+    log.warning(`Warning : DustReturned not found; Tx
